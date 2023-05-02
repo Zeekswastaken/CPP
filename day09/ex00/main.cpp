ca969fadde02855b1bed020fda806ed803c56562
yeeticus2000 <ouazahrouz@gmail.com>
2023-05-02 12:00:26 +0100

    Actual Finito

--- day09/ex00/main.cpp
+++ day09/ex00/main.cpp
@@ -1,11 +1,17 @@
 #include "BitcoinExchange.hpp"
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+    try
     {
-        std::cout << "Incorrect number of arguments" << std::endl;
-        exit(1);
+        if (argc != 2)
+        {
+            std::cout << "Incorrect number of arguments" << std::endl;
+            exit(1);
+        }
+        BitcoinExchange bt(argv[1]);
     }
-    BitcoinExchange bt(argv[1]);
-
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
